78dcea7f227cb50fb7dec975fe530f92498bb617
Ivan <ihpn1802@gmail.com>
2023-06-30 19:13:28 +0100

    update github link

--- frontend/src/pages/Footer.tsx
+++ frontend/src/pages/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p><a href="https://github.com/ivannip/onlinestore">Git Hub Source Here</a> - copyright {year} </p>
+      <p><a href="https://github.com/ivannip/my-ts-store">Git Hub Source Here</a> - copyright {year} </p>
     </footer>
     );
 };
